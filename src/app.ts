83c0b830e46c2a75e8c4268fd80d2a44a2a4d8d4
Sebastian-Simek <sebastiansimek09@gmail.com>
2023-11-08 15:55:34 -0800

    ft. added check to see if PORT exist in .env file

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,11 @@ import * as dotenv from "dotenv";
 
 dotenv.config();
 
+if (!process.env.PORT) {
+  console.log("No PORT detected!");
+  process.exit(1);
+}
+
 const PORT: number = parseInt(process.env.PORT as string, 10);
 
 const app: Express = express();
@@ -14,4 +19,5 @@ app.use(express.json());
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
+
 export default app;
