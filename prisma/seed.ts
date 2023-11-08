730b6162e23e0d9197d21087e719c0f83373693e
Sebastian-Simek <sebastiansimek09@gmail.com>
2023-11-07 17:12:05 -0800

    ft. started on seed.ts file

---
+++ prisma/seed.ts
@@ -0,0 +1,67 @@
+import { prisma } from "../src/utils/db.server";
+
+type Author = {
+  firstName: string;
+  lastName: string;
+};
+
+type Book = {
+  title: string;
+  isFiction: boolean;
+  datePublished: Date;
+};
+
+async function seed() {
+  await Promise.all(
+    getAuthors().map((author) => {
+      return prisma.authors.create({
+        data: {
+          firstName: author.firstName,
+          lastName: author.lastName,
+        },
+      });
+    })
+  );
+  const author = await prisma.authors.findFirst({
+    where: {
+      firstName: "William",
+    },
+  });
+}
+
+function getAuthors(): Array<Author> {
+  return [
+    {
+      firstName: "Anthony",
+      lastName: "Bourdain",
+    },
+    {
+      firstName: "Frank",
+      lastName: "Herbert",
+    },
+    {
+      firstName: "William",
+      lastName: "Shakespear",
+    },
+  ];
+}
+
+function getBooks(): Array<Book> {
+  return [
+    {
+      title: "Dune",
+      isFiction: true,
+      datePublished: new Date(),
+    },
+    {
+      title: "Kitchen Confidential",
+      isFiction: false,
+      datePublished: new Date(),
+    },
+    {
+      title: "Hamlet",
+      isFiction: true,
+      datePublished: new Date(),
+    },
+  ];
+}
