bf25c68896c4308d56375813cdb9bac3d07da71a
Sebastian-Simek <sebastiansimek09@gmail.com>
2023-11-09 15:21:34 -0800

    ft. added controllers and started testing

--- __tests__/authors.test.ts
+++ __tests__/authors.test.ts
@@ -1,16 +1,13 @@
 import request from 'supertest';
 import app from '../src/app';
-import seed from '../prisma/seed';
 
 describe('backend author routes', () => {
-  beforeEach(async () => {
-    await seed();
-  });
   it('adds a book and returns a status', async () => {
     const res = await request(app).post('/books ').send({
       title: 'Dune Messiah',
       isFiction: true,
       datePublished: new Date(),
+      authorId: 2,
     });
     expect(res.status).toBe(200);
   });
--- src/books/bookController.ts
+++ src/books/bookController.ts
@@ -1,2 +1,11 @@
+import { Request, Response, Router } from 'express';
+import BookService from './BookService';
 
-
+export default Router().post('/', async (req: Request, res: Response) => {
+  try {
+    const book = await BookService.createBook(req.body);
+    res.json(book);
+  } catch (error) {
+    console.error(error);
+  }
+});
